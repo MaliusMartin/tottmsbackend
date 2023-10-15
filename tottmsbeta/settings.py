dd17496f3b56aca43bc11a9f8c7b3c3405dab88e
MaliusMartin <maliberry7@gmail.com>
2023-10-16 02:34:22 +0300

    allowed host

--- tottmsbeta/settings.py
+++ tottmsbeta/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-i&0irxt$*!%ol6s^&0icc)q2&2myub2ve9!bi-as3gy932#wr3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tottmsbackend.azurewebsites.net']
 
 
 # Application definition
